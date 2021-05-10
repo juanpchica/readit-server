1248d267d2e433e2ad795f3c939a7d12dc77cb2b
juanpchica <juanpchica@hotmail.com>
2021-05-10 14:09:56 -0500

    SameSite changed to lax

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -83,7 +83,7 @@ const login = async (req: Request, res: Response) => {
       cookie.serialize("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "lax",
         maxAge: 3600,
         path: "/",
       })
