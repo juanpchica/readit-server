4f984000465e41362f67594c9c6230bc5c78725b
juanpchica <juanpchica@hotmail.com>
2021-05-10 11:52:28 -0500

    Sync false typeorm

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   type: "postgres",
   url: process.env.DATABASE_URL,
-  synchronize: true,
+  synchronize: false,
   logging: true,
   entities: ["dist/entities/**/*.js"],
   migrations: ["dist/migrations/**/*.js"],
